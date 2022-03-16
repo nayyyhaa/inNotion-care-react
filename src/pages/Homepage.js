3d5e632cb6b6631fa733b7662c475f735e3123a7
nayyyhaa <nehaguptang.ng@gmail.com>
2022-03-16 12:56:49 +0530

    add: pages + routes

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import { SiteHeader, Marquee, ProductsListing, FeatureCategories } from "../components";
+export const Homepage = () => {
+  return (
+    <>
+      <main className="full-wd">
+        <SiteHeader />
+        <Marquee />
+        <ProductsListing />
+        <FeatureCategories />
+      </main>
+    </>
+  );
+};
