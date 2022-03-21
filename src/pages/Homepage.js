7cf1d5389b3f9aad55d5d3b58fcb1b73737706b8
nayyyhaa <nehaguptang.ng@gmail.com>
2022-03-21 15:29:00 +0530

    refactor: delete unused imports

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,6 @@
 import { SiteHeader, Marquee, ProductsListing, FeatureCategories } from "../components";
-import { useCategories } from "../contexts/CategoriesContext";
-import { useProducts } from "../contexts/ProductsContext";
+
 export const Homepage = () => {
-  const { products } = useProducts();
-  const { categories } = useCategories();
   return (
     <>
       <main className="full-wd">
