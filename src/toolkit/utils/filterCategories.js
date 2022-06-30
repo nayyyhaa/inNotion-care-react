56ce6601cca9fee5adaf328438acacee75fbf925
nayyyhaa <nehaguptang.ng@gmail.com>
2022-06-30 17:49:35 +0530

    refactor: img load + searchbar

--- src/toolkit/utils/filterCategories.js
+++ src/toolkit/utils/filterCategories.js
@@ -3,4 +3,5 @@ export const filterCategories = (data, categoriesSelected) =>
     ? data.filter((it) => it.categoryName.filter((cat) => categoriesSelected.includes(cat)).length)
     : data;
 
-export const filterSearch = (data, searchIp) => (searchIp ? data.filter((it) => it.title.includes(searchIp)) : data);
+export const filterSearch = (data, searchIp) =>
+  searchIp ? data.filter((it) => it.title.toLowerCase().includes(searchIp.toLowerCase())) : data;
